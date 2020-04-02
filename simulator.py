ab3b769d932f3dd08c25956137ba10525cbf99bc
Kulneet Singh <kulneetsingh@vt.edu>
2020-04-02 05:58:19 -0400

    Simulator: added helpful comments and also made timestamp format.

--- simulator.py
+++ simulator.py
@@ -1,5 +1,6 @@
 # Neccesary libraries
 import time
+import datetime
 import paho.mqtt.client as mqtt
 import json
 from collections import defaultdict
@@ -28,6 +29,11 @@ time.sleep(1)
 ret =  client.publish("topics", "{\"id\": \"ultra\", \"pub\": 1, \"rec\": 0, \"t1\": \"\", \"t2\": \"\", \"t3\": \"\", \"t4\": \"\"}")
 time.sleep(1)
 ret =  client.publish("topics", "{\"id\": \"arm\", \"pub\": 1, \"rec\": 0, \"t1\": \"pixy\", \"t2\": \"rover\", \"t3\": \"\", \"t4\": \"\"}")
+time.sleep(1)
+
+# Pixy publishing message of current status
+timenow = datetime.datetime.now()
+ret =  client.publish("pixy", "{\"id\": \"pixy\", \"pub\": 2, \"rec\": 0, \"x_coordinate\": 10, \"y_coordinate\": 5, \"height\": 2, \"width\": 1, \"signature\": 1234, \"time\": " + str(timenow.strftime("%H%M%S")) + "}")
 time.sleep(1)
 
 # Disconnecting from cloudmqtt
